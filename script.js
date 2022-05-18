34e5d08a02d2d6e2c4b6afd5e7c6fd5ef42f0787
MatteoPellegrinii <pellegrini.matteo00@gmail.com>
2022-05-18 17:21:39 +0200

    push

--- script.js
+++ script.js
@@ -1,19 +1,63 @@
-const AngelaCarrol = {
-    nome : ['Angela Carrol'],
-    job : ['Chief editor'],
-    foto : []
+const cards = [
+    {
+        nome : 'Angela Carrol',
+        job : 'Chief editor',
+        foto : [],
+    },
+    {
+        nome : 'Angela Carrol',
+        job : 'Chief editor',
+        foto : []
+    },
 
+]
+
+console.log(cards);
+
+
+
+
+
+for (let i = 0; i < cards.length; i++) {
+    
+    // creare classe
+    let container = document.querySelector(".team-container");
+    let TeamcardClass = document.createElement("div");
+    TeamcardClass.classList.add("team-card");
+    container.append(TeamcardClass);
+
+    
+    // assegnare immagine
+    const imgELem = document.createElement("img");
+    imgELem.src = 'img/angela-caroll-chief-editor.jpg';
+    let addimage = document.createElement("div");
+    addimage.classList.add("card-image")
+    addimage.append(imgELem);
+    TeamcardClass.append(addimage);
+
+
+
+    //   assegnare nome
+    const nomeElem = document.createElement("h3");
+    TeamcardClass.append(nomeElem);
+
+
+    //   assegnare lavoro
+    const jobElem = document.createElement("p");
+    TeamcardClass.append(jobElem);
+
+    let oggettoclasseiesimo = cards[i];
+    console.log(oggettoclasseiesimo);
+    cards[i].foto.push(imgELem);
+    nomeElem.append(cards[i].nome);
+    jobElem.append(cards[i].job);
+    
+    
 }
 
-for (let key in AngelaCarrol) {
-   console.log(AngelaCarrol[key]);
 
 
-}
 
-const imgELem = document.createElement("img");
-imgELem.src = 'img/angela-caroll-chief-editor.jpg';
-let addimage = document.querySelector(".card-image");
-addimage.append(imgELem);
 
-AngelaCarrol.foto.push(imgELem);
+
+
