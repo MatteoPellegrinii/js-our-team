ff54b72abd998f99b0d9672a50c0f79ece9cabcc
MatteoPellegrinii <pellegrini.matteo00@gmail.com>
2022-05-18 15:31:40 +0200

    push

---
+++ script.js
@@ -0,0 +1,10 @@
+const employer = {
+    angelaCarrol : ['Angela Carrol','Chief Editor'],
+
+}
+
+for (let key in employer) {
+   console.log(employer[key]);
+}
+
+var images = document.querySelector("img");
